3218e7adbac8e118cb4a8f03a76218537a6419b5
IPSSandhu <ishan.sandhu3121@gmail.com>
2025-06-18 23:28:13 -0700

    completed LC 387 and another LC problem

---
+++ Arrays/find_the_diff.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        dict_s = {}
+        dict_t = {}
+
+        for x in s:
+            if x not in dict_s:
+                dict_s[x] = 1
+            elif x in dict_s:
+                dict_s[x]+=1
+        
+        for x in t:
+            if x not in dict_t:
+                dict_t[x] = 1
+            elif x in dict_t:
+                dict_t[x]+=1
+
+        
+        for key, value in dict_t.items():
+            if key in dict_s and dict_s[key] != dict_t[key]:
+                 return key
+            if key not in dict_s:
+                return key
+    
+s = Solution()
+print(s.findTheDifference("ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor",
+                          "qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj"))
+print(s.findTheDifference("abcd",
+                          "abcde"))
---
+++ Arrays/first_unique_char_LC387.py
@@ -0,0 +1,29 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d={}
+        r=""
+
+        for x in s:
+            if x not in d:
+                d[x]=1
+            elif x in d:
+                d[x]+=1
+        print(d)
+
+        for key, value in d.items():
+            if value==1:
+                r = key
+                print("firt unique char:",r)
+                return s.index(r)
+        #         break
+        # for x in range(len(s)):
+        #     if s[x]==r:
+        #         return x
+
+        return -1
+
+s = Solution()
+print(s.firstUniqChar("leetcode"))
+print(s.firstUniqChar("loveleetcode"))
+print(s.firstUniqChar("aabb"))
+            
