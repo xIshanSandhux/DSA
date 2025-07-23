fbb8f3ddd79b3c8c193ef6d54e9793981c594b6e
Ishan <ishan.sandhu3121@gmail.com>
2025-07-23 13:08:49 -0700

    completed LC 206

--- Linked List/Singly/reverse_LC206.py
+++ Linked List/Singly/reverse_LC206.py
@@ -9,5 +9,23 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        cur = head
+
+        while cur is not None:
+            next_node = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next_node
+            
         
-        return Optional[ListNode]
+        return prev
+ 
+Head = ListNode(1)
+linkedList2 = ListNode(2)
+linkedList3 = ListNode(3)
+linkedList4 = ListNode(4)
+Head.next=linkedList2
+linkedList2.next=linkedList3
+linkedList3.next=linkedList4
+print(str(Head.next.val))
